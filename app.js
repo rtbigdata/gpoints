9abfc276e012d0ea182633ff98a997f5ffff991a
Rick Ciesla <esckey71@hotmail.com>
2017-02-25 15:20:18 -0500

    port assignment to work with Heroku

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var index = require('./routes/index');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
